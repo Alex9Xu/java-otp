16884da4889ade6b8fc92372e52a5475691eb01f
Alex Xu (Xu Tao) <alex9xu@hotmail.com>
2023-11-22 09:33:17 +0800

    Update Budget.java

--- src/main/java/com/odde/budget/Budget.java
+++ src/main/java/com/odde/budget/Budget.java
@@ -1,4 +1,22 @@
 package com.odde.budget;
 
+import java.time.YearMonth;
+
 public class Budget {
+
+    private YearMonth yearMonth;
+    private int amount;
+
+    Budget(YearMonth time, int money) {
+        yearMonth = time;
+        amount = money;
+    }
+
+    public YearMonth getYearMonth() {
+        return yearMonth;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
