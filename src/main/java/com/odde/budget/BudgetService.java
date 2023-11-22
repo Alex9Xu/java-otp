afb8b33a25c6171f54554c3f4dcde76d30262fac
Alex Xu (Xu Tao) <alex9xu@hotmail.com>
2023-11-22 14:58:56 +0800

    Update BudgetService.java

--- src/main/java/com/odde/budget/BudgetService.java
+++ src/main/java/com/odde/budget/BudgetService.java
@@ -18,40 +18,10 @@ public class BudgetService {
 
         int budgetAmount = 0;
         for (Budget budgetItem : budgetList) {
-            budgetAmount += getMoneyInThisMonth(budgetItem, start, end);
+            int validDays = new TimePeriod(start, end).getDaysInPeriod(budgetItem.getYearMonth());
+            budgetAmount += budgetItem.getDailyAmount() * validDays;
         }
 
         return budgetAmount;
-    }
-
-    private int getMoneyInThisMonth(Budget repoItem, LocalDate start, LocalDate end) {
-        YearMonth startYearMonth = YearMonth.of(start.getYear(), start.getMonth());
-        YearMonth endYearMonth = YearMonth.of(end.getYear(), end.getMonth());
-
-        if (repoItem.getYearMonth().isBefore(startYearMonth))
-            return 0;
-        else if (repoItem.getYearMonth().isAfter(endYearMonth))
-            return 0;
-        else if (repoItem.getYearMonth().equals(startYearMonth))
-            return getMoneyInStart(start, repoItem);
-        else if (repoItem.getYearMonth().equals(endYearMonth))
-            return getMoneyInEnd(end, repoItem);
-        else
-            return repoItem.getAmount();
-    }
-
-    private int getMoneyInStart(LocalDate start, Budget repoItem) {
-        if (start.getDayOfMonth() == 1)
-            return repoItem.getAmount();
-
-        YearMonth yearMonth = YearMonth.of(start.getYear(), start.getMonth());
-        int validDays = yearMonth.lengthOfMonth() - start.getDayOfMonth() + 1;
-
-        return repoItem.getDailyAmount() * validDays;
-    }
-
-    private int getMoneyInEnd(LocalDate end, Budget repoItem) {
-        int validDays = end.getDayOfMonth();
-        return repoItem.getDailyAmount() * validDays;
     }
 }
